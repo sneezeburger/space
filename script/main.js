30e8948e9d4cc8aa3fee6890a1870a4a4de34cdc
Adam Taylor <adam.douglas.taylor@gmail.com>
2017-08-01 21:55:32 -0700

    Change "ship" style from id to class Clean up CSS (remove unnecessary positioning) Remove static ship and invader from index.html Move <script> tag from body to head Clamp ship to right side of window Spawn shot above ship instead of inside of it Dynamically create invaders! Dynamically create ship. Move various things from global scope to onload function Hit detection between shots and invaders!

--- script/main.js
+++ script/main.js
@@ -1,5 +1,5 @@
 function onkeydown(e){
-	switch(e.key){ // TODO more efficient to check keycode?
+		switch(e.key){ // TODO more efficient to check keycode?
 		case 'ArrowRight':
 			moveright=1;
 			break;
@@ -8,7 +8,7 @@ function onkeydown(e){
 			break;
 		case ' ':
 		case 'ArrowUp':
-			if(!shooting){
+			if(!shooting){ // TODO may need to cap number of shots
 				shooting=1;
 				shoot();
 			}
@@ -45,13 +45,23 @@ function tick(){
 	}
 	if(delta){
 		ship.style.left=String(Math.max(0,getElementLeft(ship)+delta))+'px';
+		ship.style.left=String(Math.min(document.body.clientWidth-ship.offsetWidth,getElementLeft(ship)))+'px'; // TODO not quite right
 	}
-	for(i=0;i<shots.length;i++){
+	for(i=0;i<shots.length;++i){ // TODO shots.foreach?
 		if(shots[i].style.display=='none'){
 			shots[i].parentNode.removeChild(shots[i]);
 			shots.splice(i,1);
 		}else{
 			animateShot(elapsedSec, shots[i]);
+		}
+	}
+
+	for(i=0;i<shots.length;++i){ // TODO shots.foreach?
+		for(j=0;j<invaders.length;++j){
+			if(overlap(shots[i],invaders[j])){
+				shots[i].style.display='none';
+				invaders[j].style.display='none'; // TODO add a HTML5 canvas and use it to draw fancy particle explosions
+			}
 		}
 	}
 	this.previousTime=thisTickTime;
@@ -64,7 +74,8 @@ function shoot(){
 	newShot.appendChild(text);
 	document.body.appendChild(newShot);
 	newShot.style.left=String(getElementLeft(ship))+'px';
-	newShot.style.top=String(getElementTop(ship))+'px';
+	newShot.style.top=String(getElementTop(ship)-ship.offsetHeight)+'px'; // TODO not quite right
+
 	shots.push(newShot);
 }
 
@@ -84,25 +95,67 @@ function getElementTop(element){
 	return parseFloat(document.defaultView.getComputedStyle(element).top);
 }
 
-document.onkeydown=onkeydown;
-document.onkeyup=onkeyup;
-document.onload=function(e){ // TODO why isn't this working
-	tick.previousTime=Date.now();
-	console.log('window.onload via anonymous fn',e,Date.now());
+function createInvaders(){
+	for(i=1;i<=12;++i){
+		for(j=1;j<=5;++j){
+			var newInvader=document.createElement('div');
+			newInvader.classList.add('invader');
+			var text=document.createTextNode("$");
+			newInvader.appendChild(text);
+			document.body.appendChild(newInvader);
+			newInvader.style.left=String(i*50)+'px';
+			newInvader.style.top=String(j*50)+'px';
+			invaders.push(newInvader);
+		}
+	}
 }
+
+function createShip(){
+	ship=document.createElement('div');
+	ship.classList.add('ship');
+	var text=document.createTextNode("X");
+	ship.appendChild(text);
+	ship.style.left='50%';
+	ship.style.bottom='40px';
+	document.body.appendChild(ship);
+}
+
+function onload(e){
+	document.onkeydown=onkeydown;
+	document.onkeyup=onkeyup;
+	score=document.getElementById("score");
+	tick.previousTime=Date.now();
+	tickTimer=setInterval(tick,tickInterval); // TODO null this on game over
+	createInvaders();
+	createShip();
+}
+
+function overlap(a,b){
+	var rectA=a.getBoundingClientRect();
+	var rectB=b.getBoundingClientRect();
+	return !(rectA.right < rectB.left || 
+		rectA.left > rectB.right || 
+ 		rectA.bottom < rectB.top || 
+		rectA.top > rectB.bottom);
+}
+
+// TODO make this all OO with these globals instead members of a singleton GameState class
+var tickTimer;
+
 // Elements
-var ship=document.getElementById("ship");
-var score=document.getElementById("score");
+var ship;
+var score;
 
 // States
 var shooting=0;
 var moveright=0;
 var moveleft=0;
 var shots=[]; // holds HTML shot elements
+var invaders=[]; // holds HTML invader elements
 
 // Configurables
 var tickInterval=10;
 var shipSpeedPixelsPerSec=150;
 var shotSpeedPixelsPerSec=450;
 
-var tickTimer=setInterval(tick,tickInterval); // TODO null this on game over
+window.addEventListener('load',onload);
